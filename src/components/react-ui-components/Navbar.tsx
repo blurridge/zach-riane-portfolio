9090d7e3a0326c6dea2934fb030131ed8d00ef0c
Zach Riane Machacon <zachriane01@gmail.com>
2024-04-10 23:50:21 +0800

    feat: created hamburger nav

--- src/components/react-ui-components/Navbar.tsx
+++ src/components/react-ui-components/Navbar.tsx
@@ -1,28 +1,114 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { isMobile, isMobileMenuOpen, isTop } from "@/stores/store";
+import { useStore } from "@nanostores/react";
+import { Menu } from "lucide-react";
+import { Transition } from "@headlessui/react";
+import zrmLogo from "@/images/zrm_logo.svg";
+import { X } from "lucide-react";
 
-interface NavbarProps {
-  children: ReactNode;
-}
+export const NavbarComponent = () => {
+  const top = useStore(isTop);
+  const mobile = useStore(isMobile);
+  const mobileMenu = useStore(isMobileMenuOpen);
 
-export const NavbarComponent = ({ children }: NavbarProps) => {
-  const [top, setTop] = useState(true);
+  const toggleMenu = () => {
+    isMobileMenuOpen.set(!mobileMenu);
+  };
 
-  useEffect(() => {
-    const scrollHandler = () => {
-      window.scrollY > 10 ? setTop(false) : setTop(true);
-    };
-    window.addEventListener("scroll", scrollHandler);
-    return () => window.removeEventListener("scroll", scrollHandler);
-  }, [top]);
-  return (
-    <>
+  if (mobile) {
+    return (
+      <nav
+        className={`fixed top-0 left-0 right-0 z-50 w-full bg-white ${
+          !top && `shadow-lg`
+        }`}
+      >
+        <div className="flex justify-between">
+          <a href="#">
+            <img
+              className="ml-2"
+              src={zrmLogo.src}
+              width="100"
+              alt="Zach Riane Machacon logo"
+            />
+          </a>
+          <button onClick={toggleMenu} className="p-4">
+            {mobileMenu ? <X /> : <Menu />}
+          </button>
+        </div>
+        <Transition
+          show={mobileMenu}
+          enter="transition ease duration-500 transform"
+          enterFrom="opacity-0 z-[-1] absolute left-0 right-0 top-[-100vh]"
+          enterTo="opacity-100 z-[-1] absolute left-0 right-0 top-0"
+          leave="transition ease duration-500 transform"
+          leaveFrom="opacity-100 z-[-1] absolute left-0 right-0 top-0"
+          leaveTo="opacity-0 z-[-1] absolute left-0 right-0 top-[-100vh]"
+        >
+          <div className="z-[-1] absolute left-0 right-0 top-0 h-screen gap-10 flex flex-col text-2xl font-semibold justify-center items-center bg-white border-t-2 shadow-md">
+            <a href="#" className="hover:text-gray-600" onClick={toggleMenu}>
+              Home
+            </a>
+            <a
+              href="#about"
+              className="hover:text-gray-600"
+              onClick={toggleMenu}
+            >
+              About
+            </a>
+            <a
+              href="#experience"
+              className="hover:text-gray-600"
+              onClick={toggleMenu}
+            >
+              Experience
+            </a>
+            <a
+              href="#projects"
+              className="hover:text-gray-600"
+              onClick={toggleMenu}
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="hover:text-gray-600"
+              onClick={toggleMenu}
+            >
+              Contact
+            </a>
+          </div>
+        </Transition>
+      </nav>
+    );
+  } else {
+    return (
       <nav
         className={`fixed top-0 left-0 right-0 z-50 w-full flex justify-between bg-white ${
           !top && `shadow-lg`
         }`}
       >
-        {children}
+        <a href="#">
+          <img
+            className="ml-2"
+            src={zrmLogo.src}
+            width="100"
+            alt="Zach Riane Machacon logo"
+          />
+        </a>
+        <div className="flex items-center justify-center gap-10 font-semibold text-lg mr-10">
+          <a href="#about" className="hover:text-gray-600">
+            About
+          </a>
+          <a href="#experience" className="hover:text-gray-600">
+            Experience
+          </a>
+          <a href="#projects" className="hover:text-gray-600">
+            Projects
+          </a>
+          <a href="#contact" className="hover:text-gray-600">
+            Contact
+          </a>
+        </div>
       </nav>
-    </>
-  );
+    );
+  }
 };
