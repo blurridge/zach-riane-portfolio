0c296845666b8a86cf256db63a84005630e736e1
Zach Riane Machacon <zachriane01@gmail.com>
2024-04-12 11:22:14 +0800

    fix: redid experience and fixed gradients across screens

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,12 +7,6 @@ module.exports = {
       fontSize: {
         "2xs": ["0.65rem", { lineHeight: "1rem" }],
       },
-      backgroundImage: {
-        "right-gradient":
-          "linear-gradient(90deg, rgba(79, 16, 145, 1) 0%, rgba(255, 255, 255, 1) 50%)",
-        "left-gradient":
-          "linear-gradient(270deg, rgba(79, 16, 145, 1) 0%, rgba(255, 255, 255, 1) 50%)",
-      },
       colors: {
         "light-purple": "#C7BEF5",
         "light-blue": "#EDF3FE",
