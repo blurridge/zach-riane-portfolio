350f1267d29427a9d3b75dbb0a5ead18fa2ed4d4
Zach Riane Machacon <zachriane01@gmail.com>
2025-03-15 14:30:38 +0800

    feat: converted react components to astro

--- src/stores/greetingStore.ts
+++ src/stores/greetingStore.ts
@@ -23,37 +23,6 @@ export const typingEffectConfig = {
   typingSpeed: 200,
 };
 
-export const handleTypingEffect = () => {
-  if (
-    typingSubIndex.get() === greetings[typingIndex.get()].length + 1 &&
-    !typingReverse.get()
-  ) {
-    // Wait before starting to delete the text
-    setTimeout(() => typingReverse.set(true), 2000);
-    return () => {}; // Return an empty function to avoid 'undefined' being returned
-  }
-
-  if (typingSubIndex.get() === 0 && typingReverse.get()) {
-    // Immediately after finishing deleting, start typing the next greeting
-    typingReverse.set(false);
-    const newIndex = (typingIndex.get() + 1) % greetings.length;
-    typingIndex.set(newIndex);
-    return () => {}; // Return an empty function to avoid 'undefined' being returned
-  }
-
-  const timeout = setTimeout(() => {
-    const newSubIndex = typingSubIndex.get() + (typingReverse.get() ? -1 : 1);
-    typingSubIndex.set(newSubIndex);
-  }, Math.max(typingReverse.get() ? 75 : typingEffectConfig.typingSpeed, Math.random() * 350));
-
-  return () => clearTimeout(timeout);
-};
-
 export const handleTypingBlink = () => {
   typingBlink.set(!typingBlink.get());
-};
-
-// Set up listeners
-export const initializeTypingEffect = () => {
-  return setInterval(handleTypingBlink, 500);
 };
